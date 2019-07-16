d9b724882ac3570801f9d305e78aff0e3d4b712d
rails <rails@railss-Mac-mini-2.local>
2019-07-16 10:40:20 +0530

    Initial Commit

---
+++ ticTacToeTejas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ticTacToeTejas
+//
+//  Created by rails on 16/07/19.
+//  Copyright © 2019 rails. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
